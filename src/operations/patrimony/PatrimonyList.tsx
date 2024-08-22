42c97ec40bd3ba0d8cbaac2ac87d2f1a1d98d6ae
lianafinaritra <hei.lianafinaritra@gmail.com>
2024-08-22 14:18:05 +0300

    feat: add typography to patrimony list

--- src/operations/patrimony/PatrimonyList.tsx
+++ src/operations/patrimony/PatrimonyList.tsx
@@ -5,7 +5,8 @@ import {
   List,
   TextField,
 } from 'react-admin';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import { HR_COLOR } from '@/HrTheme';
 
 export const IMPORT_BUTTON_STYLE = {
   width: '8rem',
@@ -17,20 +18,49 @@ export const PatrimonyList = () => {
     <>
       <Box
         sx={{
-          marginTop: 1,
+          marginTop: 5,
+          marginBottom: 1,
           border: 0,
           height: 60,
           display: 'flex',
-          justifyContent: 'flex-start',
-          paddingLeft: 2,
-          alignItems: 'center',
+          flexDirection: 'row',
         }}
       >
-        <CreateButton
-          data-testid="create-button"
-          label="Créer"
-          sx={IMPORT_BUTTON_STYLE}
-        />
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            width: '50%',
+            paddingLeft: 2,
+          }}
+        >
+          <Typography
+            align="center"
+            mr={12}
+            variant="h6"
+            fontWeight="bold"
+            color={HR_COLOR[30]}
+          >
+            Liste des patrimoines
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            width: '50%',
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+          }}
+        >
+          <CreateButton
+            data-testid="create-button"
+            label="Créer"
+            sx={IMPORT_BUTTON_STYLE}
+          />
+        </Box>
       </Box>
       <List resource="patrimoines" actions={false}>
         <Datagrid bulkActionButtons={false}>
