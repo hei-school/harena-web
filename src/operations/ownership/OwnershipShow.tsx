14fc53098066e34330f078ac7c670249f6a02059
lianafinaritra <hei.lianafinaritra@gmail.com>
2024-09-02 10:20:07 +0300

    fix: ownership-edition-redirection

--- src/operations/ownership/OwnershipShow.tsx
+++ src/operations/ownership/OwnershipShow.tsx
@@ -58,7 +58,7 @@ const PossessionShowActions = () => {
         }}
       >
         <Button
-          href={`/#/patrimoines/${patrimoineNom}/possessions/${possessionNom}/edit`}
+          href={`/patrimoines/${patrimoineNom}/possessions/${possessionNom}/edit`}
           startIcon={<EditIcon />}
           label="Modifier"
           variant="contained"
